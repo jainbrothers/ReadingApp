08f436b53039d4b6a213a14804fb23e116295cf6
Ajit Jain <jainajit@amazon.com>
2020-07-05 06:55:03 +0530

    Added audio permission in manifest and disabled google mic in a rudimentory way.

--- app/src/main/java/com/example/speechtotextfromgitrepo/activity/MainActivity.java
+++ app/src/main/java/com/example/speechtotextfromgitrepo/activity/MainActivity.java
@@ -12,7 +12,10 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
@@ -26,72 +29,115 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private TextView txvResult;
+    private SpeechRecognizer sr;
+    private String TAG = "MainActivityLoggging";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("basic logging");
-        getSpeechInput();
+
         txvResult = (TextView) findViewById(R.id.txvResult);
-    }
 
-    public void getSpeechInput() {
 
-        System.out.println("inside getSpeechInput");
+        sr = SpeechRecognizer.createSpeechRecognizer(this);
+        sr.setRecognitionListener(new listener());
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-
-        try {
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                System.out.println("before starting error logging");
-                startActivityForResult(intent, 10);
-                System.out.println("after starting error logging");
-            } else {
-                Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
-            }
-        } catch (Exception ex) {
-            System.out.println("exception received while starting activity. " + ex.getMessage());
-        }
-
-        System.out.println("exiting getSpeechInput");
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"voice.recognition.test");
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,5);
+        sr.startListening(intent);
     }
 
-//    public void getSpeechInput(View view) {
+    class listener implements RecognitionListener
+    {
+        public void onReadyForSpeech(Bundle params)
+        {
+            Log.d(TAG, "onReadyForSpeech");
+        }
+        public void onBeginningOfSpeech()
+        {
+            Log.d(TAG, "onBeginningOfSpeech");
+        }
+        public void onRmsChanged(float rmsdB)
+        {
+            Log.d(TAG, "onRmsChanged");
+        }
+        public void onBufferReceived(byte[] buffer)
+        {
+            Log.d(TAG, "onBufferReceived");
+        }
+        public void onEndOfSpeech()
+        {
+            Log.d(TAG, "onEndofSpeech");
+        }
+        public void onError(int error)
+        {
+            Log.d(TAG,  "error " +  error);
+            txvResult.setText("error " + error);
+        }
+        public void onResults(Bundle results)
+        {
+            String str = new String();
+            Log.d(TAG, "onResults " + results);
+            ArrayList<String> data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < data.size(); i++)
+            {
+                Log.d(TAG, "result " + data.get(i));
+                str += data.get(i);
+            }
+            txvResult.setText(data.get(0));
+        }
+        public void onPartialResults(Bundle partialResults)
+        {
+            Log.d(TAG, "onPartialResults");
+        }
+        public void onEvent(int eventType, Bundle params)
+        {
+            Log.d(TAG, "onEvent " + eventType);
+        }
+    }
+
+
+
+//    public void getSpeechInput() {
 //
-//        Intent intent = new Intent(RecognizerIntent.ACTION_VOICE_SEARCH_HANDS_FREE);
+//        System.out.println("inside getSpeechInput");
+//        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 //        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
 //        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 //
 //        try {
-//            System.out.println("basic logging");
 //            if (intent.resolveActivity(getPackageManager()) != null) {
 //                System.out.println("before starting error logging");
 //                startActivityForResult(intent, 10);
+//                System.out.println("after starting error logging");
 //            } else {
 //                Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
 //            }
 //        } catch (Exception ex) {
 //            System.out.println("exception received while starting activity. " + ex.getMessage());
 //        }
+//
+//        System.out.println("exiting getSpeechInput");
 //    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        System.out.println(" requestCode " + requestCode + " resultCode " + resultCode + " data " + data);
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case 10:
-                if (resultCode == RESULT_OK && data != null) {
-                    System.out.println("Received result from callback. data %s " + data.toString());
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    txvResult.setText(result.get(0));
-                    getSpeechInput();
-                    System.out.println("call getSpeechInput afer receiving result");
-                }
-                break;
-        }
-    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        System.out.println(" requestCode " + requestCode + " resultCode " + resultCode + " data " + data);
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        switch (requestCode) {
+//            case 10:
+//                if (resultCode == RESULT_OK && data != null) {
+//                    System.out.println("Received result from callback. data %s " + data.toString());
+//                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                    txvResult.setText(result.get(0));
+//                    getSpeechInput();
+//                    System.out.println("call getSpeechInput afer receiving result");
+//                }
+//                break;
+//        }
+//    }
 }
