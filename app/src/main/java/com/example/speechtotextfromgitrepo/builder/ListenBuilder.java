07455914be9dd2507839cbf4e9f11291631963c3
eijarah <rahul.f.jain@ericsson.com>
2020-07-10 23:28:07 +0530

    adding - RecognizerIntent.EXTRA_LANGUAGE, en-IN

--- app/src/main/java/com/example/speechtotextfromgitrepo/builder/ListenBuilder.java
+++ app/src/main/java/com/example/speechtotextfromgitrepo/builder/ListenBuilder.java
@@ -21,8 +21,10 @@ public class ListenBuilder {
 
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-IN");
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,5);
+
         speechRecognizer.startListening(intent);
-        speechRecognizer.stopListening();
+//        speechRecognizer.stopListening();
     }
 }
